db25e0119a54496ea85b757c4c197a9b874e32a9
yday <yangdayeon2@gmail.com>
2022-03-15 12:15:27 +0900

    namespace로 축약

--- C++기본/00 C++기본.cpp
+++ C++기본/00 C++기본.cpp
@@ -1,9 +1,12 @@
 //std::cout을 포함
 #include <iostream>
+//std::를 생략(이거 안 쓰는 사람들도 많음)
+using namespace std;
 
 int main(void)
 {
 	//std::cout 객체로 문자열을 출력
-	std::cout << "안녕 세상아\n";
+	//std::endl 객체로 개행처리
+	cout << "안녕 세상아" << endl;
 	return 0;
 }
