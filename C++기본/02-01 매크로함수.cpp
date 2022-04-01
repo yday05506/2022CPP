c984397967ebd6fa0c8b30f75ace0a22e1f9825d
yday <yangdayeon2@gmail.com>
2022-04-01 10:07:59 +0900

    매크로함수 완성

--- C++기본/02-01 매크로함수.cpp
+++ C++기본/02-01 매크로함수.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#define jegob(A) A * A
+#define jegob(A) ((A) * (A))
 
 using namespace std;
 
 int main(void)
 {
 	cout << jegob(3 + 1) << endl;	//4*4가 아닌 의도치 않은 결과
-	cout << 3 + 1 * 3 + 1 << endl;	//위의 코드와 똑같음
+	cout << ((3 + 1) * (3 + 1)) << endl;	//위의 코드와 똑같음
 
 	return 0;
 }
