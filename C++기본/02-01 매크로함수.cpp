839c06401947fe1dd46017bea7769de1f9262568
yday <yangdayeon2@gmail.com>
2022-04-01 10:05:45 +0900

    매크로함수 오류결과

--- C++기본/02-01 매크로함수.cpp
+++ C++기본/02-01 매크로함수.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main(void)
 {
-	cout << jegob(3) << endl;
-	cout << 3 * 3 << endl;	//위의 코드와 똑같음
+	cout << jegob(3 + 1) << endl;	//4*4가 아닌 의도치 않은 결과
+	cout << 3 + 1 * 3 + 1 << endl;	//위의 코드와 똑같음
 
 	return 0;
 }
