6daafbb16335e5bb6a19c216daa122dc21474363
yday <yangdayeon2@gmail.com>
2022-04-05 11:37:23 +0900

    Call by reference

--- 02-04 참조2.cpp
+++ 02-04 참조2.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-void plus2(int* num)
+void plus2(int& num)
 {
 	// 포인터 변수이기에 내부값을 *로 써서 처리
-	*num += 2;
+	num += 2;
 }
 
 int main(void)
 {
 	int a = 3;
 	// 포인터 매개변수 num에게 주소값으로 전달
-	plus2(&a);
+	plus2(a);
 	cout << a << endl;
 }
