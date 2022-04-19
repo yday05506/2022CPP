423f4e0c058e40066c0b0fce06e070a433b6f917
yday <yangdayeon2@gmail.com>
2022-04-19 12:12:07 +0900

    생성자 오버로딩

--- 03-01 class 생성자.cpp
+++ 03-01 class 생성자.cpp
@@ -14,6 +14,8 @@ public:
 	// 생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	// 반환형을 갖고 있지 않는다
 	Student();
+	Student(int Hakbun, string Name);
+
 	void show();
 };
 
@@ -23,18 +25,29 @@ Student::Student()
 	sName = "이사랑";
 	cout << "학번이 등록되었습니다." << endl;
 }
+
+Student::Student(int Hakbun, string Name) 
+{
+	nHakbun = Hakbun;
+	sName = Name;
+	cout << "학번이 등록되었습니다." << endl;
+}
+
 void Student::show()
 {
 	cout << "학번은 " << nHakbun << "입니다." << endl;
-	cout << "이름은 " << sName << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;
 }
 
 int main(void)
 {
 	// 기본생성자
-	Student stu;
+	Student stu1 = Student();
 	//Student student2 = Student();
-	stu.show();
+	stu1.show();
+
+	Student stu2 = Student(1111, "YDY");
+	stu2.show();
 
 	return 0;
 }
