372f547b44ddd07799ef45d4ffaab0e398da9dd4
yday <yangdayeon2@gmail.com>
2022-05-13 10:13:04 +0900

    객체배열 동적할당

--- 03-01 class 생성자.cpp
+++ 03-01 class 생성자.cpp
@@ -23,14 +23,14 @@ Student::Student()
 {
 	this->nHakbun = 1234;
 	this->sName = "이사랑";
-	cout << "학번이 등록되었습니다." << endl;
+	cout << "일반생성자 생성." << endl;
 }
 
 // 멤버변수를 초기화 할 수 있으며 const형 변수와 참조형 변수를 멤버변수로 할 수 있다
 Student::Student(int Hakbun, string Name)
 	: nHakbun(Hakbun), sName(Name)	// 멤버변수(매개변수)
 {
-	cout << "학번이 등록되었습니다." << endl;
+	cout << "일반생성자 생성." << endl;
 }
 
 void Student::show()
@@ -41,10 +41,12 @@ void Student::show()
 
 int main(void)
 {
-	Student* stu2 = new Student(1111, "YDY");
-	stu2->show();
+	Student* stu2 = new Student[6];
+	for (int i = 0; i < 6; i++) {
+		stu2[i].show();
+	}
 
-	delete stu2;
+	delete []stu2;
 
 	return 0;
 }
