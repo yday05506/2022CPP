4f8b6352d49c0456c83b19f0c210fce12485a32e
yday <yangdayeon2@gmail.com>
2022-05-13 09:17:56 +0900

    일반 변수 | 동적 할당

--- 03-01 class 생성자.cpp
+++ 03-01 class 생성자.cpp
@@ -41,13 +41,20 @@ void Student::show()
 
 int main(void)
 {
-	// 기본생성자
-	Student stu1 = Student();
-	//Student student2 = Student();
-	stu1.show();
+	//// 기본생성자
+	//Student stu1 = Student();
+	////Student student2 = Student();
+	//stu1.show();
 
-	Student stu2 = Student(1111, "YDY");
-	stu2.show();
+	//Student stu2 = Student(1111, "YDY");
+	//stu2.show();
+
+	int* ptr1 = new int;
+	*ptr1 = 20;
+
+	cout << *ptr1 << endl;
+
+	delete ptr1;
 
 	return 0;
 }
