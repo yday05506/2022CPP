aa3b36ed8ffe4f40de1d722404b563aa5ad5db6a
yday <yangdayeon2@gmail.com>
2022-05-17 11:17:58 +0900

    string 대신에 전통적인 방식으로 생성자 변경

--- 03-01 class 생성자.cpp
+++ 03-01 class 생성자.cpp
@@ -1,5 +1,5 @@
 #include <iostream>	// cout, endl 사용
-#include <string>
+#include <string.h>	// strcpy() 사용
 
 using namespace std;
 
@@ -7,30 +7,28 @@ class Student
 {
 private :
 	int nHakbun;
-	string sName;
+	char* sName;
 	
 public:
 	// 아래와 같은 기본생성자를 컴파일러가 만들어줌
 	// 생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	// 반환형을 갖고 있지 않는다
 	Student();
-	Student(int Hakbun, string Name);
+	Student(int Hakbun, const char* Name);
 
 	void show();
 };
 
-Student::Student()
-{
-	this->nHakbun = 1234;
-	this->sName = "이사랑";
-	cout << "일반생성자 생성." << endl;
-}
+Student::Student() {}
 
 // 멤버변수를 초기화 할 수 있으며 const형 변수와 참조형 변수를 멤버변수로 할 수 있다
-Student::Student(int Hakbun, string Name)
-	: nHakbun(Hakbun), sName(Name)	// 멤버변수(매개변수)
+Student::Student(int Hakbun, const char* Name)
+	: nHakbun(Hakbun)	// 멤버변수(매개변수)
 {
-	cout << "일반생성자 생성." << endl;
+	cout << "일반생성자 호출." << endl;
+	int len = strlen(Name) + 1;	// 공간의 개수 파악
+	sName = new char[len];	// 개수만큼 메모리 할당
+	strcpy(sName, Name);
 }
 
 void Student::show()
@@ -41,10 +39,8 @@ void Student::show()
 
 int main(void)
 {
-	for (int i = 0; i < 10000; i++) {
-		Student* stu2 = new Student[1000];
-		delete[]stu2;
-	}
+	Student stu1 = Student(1111, "YDY");
+	stu1.show();
 
 	return 0;
 }
