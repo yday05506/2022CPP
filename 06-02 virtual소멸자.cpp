42e6a44a01c9bd8b17fb6bd3b0262613b17bb9e2
yday <yangdayeon2@gmail.com>
2022-08-30 11:51:49 +0900

    소멸자 앞에는 무조건 virtual 키워드 넣기

--- 06-02 virtual소멸자.cpp
+++ 06-02 virtual소멸자.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Animal {
 public:
 	Animal() { cout << "Animal 积己" << endl; }
-	~Animal() { cout << "Animal 家戈" << endl; }
+	virtual ~Animal() { cout << "Animal 家戈" << endl; }
 	void eat(void) { cout << "悼拱 冈绢" << endl; }
 
 private:
@@ -15,7 +15,7 @@ private:
 class Tiger : public Animal {
 public:
 	Tiger() { cout << "Tiger 积己" << endl; }
-	~Tiger() { cout << "Tiger 家戈" << endl; }
+	virtual ~Tiger() { cout << "Tiger 家戈" << endl; }
 };
 
 void main(void) {
