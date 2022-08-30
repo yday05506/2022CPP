c8a5e1e67a8c2cb17038fef32cadbfba7a08d046
yday <yangdayeon2@gmail.com>
2022-08-30 11:14:32 +0900

    가상함수를 통한 동적바인딩(다형성 진행)

--- 06-01 다형성기본.cpp
+++ 06-01 다형성기본.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Animal {
 public:
 	void eat(void) { cout << "µ¿¹° ¸Ô¾î" << endl; }
-	void roar(void) { cout << "µ¿¹° Â¢¾î" << endl; }
+	virtual void roar(void) { cout << "µ¿¹° Â¢¾î" << endl; }
 	void walk(void) { cout << "µ¿¹° °É¾î" << endl; }
 	
 private:
@@ -17,12 +17,12 @@ private:
 
 class Tiger : public Animal {
 public:
-	void roar() { cout << "¾îÈï" << endl; }
+	void roar() override { cout << "¾îÈï" << endl; }
 };
 
 class Dog : public Animal {
 public:
-	void roar() { cout << "¸Û¸Û" << endl; }
+	void roar() override { cout << "¸Û¸Û" << endl; }
 };
 
 void main(void) {
@@ -31,10 +31,10 @@ void main(void) {
 	delete animal;
 
 	animal = new Tiger();
-	animal->roar();	// "µ¿¹° Â¢¾î" (¾îÈï X)
+	animal->roar();	// "¾îÈï"
 	delete animal;
 
 	animal = new Dog();
-	animal->roar();	// "µ¿¹° Â¢¾î" (¸Û¸Û X)
+	animal->roar();	// "¸Û¸Û"
 	delete animal;
 }
