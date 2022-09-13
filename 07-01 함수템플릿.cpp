72925fdc89b3c68df9de998fb5e917920c86f502
yday <yangdayeon2@gmail.com>
2022-09-13 10:58:45 +0900

    템플릿 적용

--- 07-01 함수템플릿.cpp
+++ 07-01 함수템플릿.cpp
@@ -3,27 +3,19 @@
 
 using namespace std;
 
+template <typename T>
+T sum(T a, T b) {
+	return a + b;
+}
 int sum(int a, int b);
-float sum(float a, float b);
-string sum(string a, string b);
 
 int main(void) {
-	cout << sum(1, 2) << endl;
-	cout << sum(1.f, 2.f) << endl;
-	cout << sum("1", "2") << endl;
+	cout << sum<int>(1, 2) << endl;
+	cout << sum<float>(1.f, 2.f) << endl;
+	cout << sum<string>("1", "2") << endl;
 }
 
 int sum(int a, int b)
-{
-	return a + b;
-}
-
-float sum(float a, float b)
-{
-	return a + b;
-}
-
-string sum(string a, string b)
 {
 	return a + b;
 }
