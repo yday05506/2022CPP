310e23412598d1d1952f03179c293ee3b6a095a4
yday <yangdayeon2@gmail.com>
2022-09-15 21:56:13 +0900

    이름, 체력, 공격, 방어 출력(동적할당) -> 템플릿 적용 전(수정)

---
+++ 코드짜보기_템플릿 적용 전.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Character {
+public:
+	Character(string na, int hp, int at, int de)
+		:name(na), HP(hp), attack(at), defense(de) {}
+
+	void Situation() {
+		cout << "name : " << name << endl;
+		cout << "HP : " << HP << endl;
+		cout << "attack : " << attack << endl;
+		cout << "defense : " << defense << endl;
+	}
+
+private:
+	string name;
+	int HP;
+	int attack;
+	int defense;
+};
+
+int main(void) {
+	Character* character = new Character("AAA", 10, 5, 3);
+	character->Situation();
+
+	delete character;
+
+	return 0;
+}
