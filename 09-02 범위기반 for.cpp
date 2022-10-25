28872bafc1641cb29b984eee976b27070ed492c9
yday <yangdayeon2@gmail.com>
2022-10-25 10:49:14 +0900

    전통적인 반복문

---
+++ 09-02 범위기반 for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void main(void)
+{
+	int arr[4] = { 10,20,30,40 };
+
+	for (int i = 0; i < sizeof(arr)/sizeof(int); i++) {
+		cout << arr[i];
+	}
+}
